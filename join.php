5b9cf023c90951dc9cd7153b219b6e8962055475
ikhlas ahmed <ikhlas_06@hotmail.com>
2013-01-04 13:55:38 -0500

    updated sign up

--- join.php
+++ join.php
@@ -11,12 +11,35 @@
 <div id='full_content'>
 	<?php include("left-menu.php"); ?>
     <div id='join-content' class='content'>
+    	<h1>SIGN UP</h1>
+        <h2>IT'S FREE</h2>
     	<form action='join.php' method='POST'>
-        	<input type='field' id='first-name-field' class='field' name='first-name'/>
-        	<input type='field' id='first-name-field' class='field' name='first-name'/>
-            <input type='field' id='first-name-field' class='field' name='first-name'/>
-            <input type='field' id='first-name-field' class='field' name='first-name'/>
-                    
+        	<p>
+        		<input type='field' placeholder='My First Name' id='first-name-field' class='field' name='first-name'/>
+                <input type='field' placeholder='My Last Name' id='first-name-field' class='field' name='first-name'/>
+            </p>
+            <p>
+        		<input type='field' placeholder='My Email Address' id='first-name-field' class='field' name='first-name'/>
+            </p>
+            <p>
+        		<input type='field' placeholder='Retype  First Name' id='first-name-field' class='field' name='first-name'/>
+            </p>
+            <p>
+        		<input type='field' id='first-name-field' class='field' name='first-name'/>
+            </p>
+            <p>
+        		<input type='field' id='first-name-field' class='field' name='first-name'/>
+            </p>
+            <p>
+                <input type="radio" id='femaler-radio' class='radio' name="gender"  value="female"> <label>Female</label>
+                <input type="radio" id='male-radio' class='radio' name="gender"  value="male"> <label>Male</label>
+           </p> 
+           <p>
+           		By clicking Sing Up, you agree to our <a href='#'>Terms</a> and that you have read our <a href='#'>Anti-Scam Policy</a>
+           </p>
+           <p>
+           		<input type="submit" i class='button' d='button_signup' name="signup" >Signup</button>
+           </p>  
         </form>
     </div>
     
