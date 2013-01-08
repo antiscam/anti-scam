4d394ec846562e68468e81d56a3ea0e4923d2e56
ikhlas ahmed <ikhlas_06@hotmail.com>
2013-01-08 17:32:32 -0500

    r

--- left-menu.php
+++ left-menu.php
@@ -2,7 +2,7 @@
 	echo"
 		<div id='menu'>
 			<a href='index.php' class='button button_menu'>Home</a>
-			<a href='#' class='button button_menu'>Report</a>
+			<a href='repost.php' class='button button_menu'>Report</a>
 			<a href='#' class='button button_menu'>Write Tips</a>
 			 <a href='#' class='button button_menu'>Search</a>
 			 <a href='aboutus.php' class='button button_menu'>About Us</a>		   
