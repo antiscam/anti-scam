9178881658b28e9043cf04d0f10aa099db3396f3
wu <panwu5588@yahoo.com>
2013-01-08 20:45:03 -0500

    fiexed minor error in the report honesty page

--- report.php
+++ report.php
@@ -83,26 +83,34 @@
                             <form action='report_scam.php' method='POST'>                            
                                 <h2>Opponent's Information:</h2> 
                                 <p>
-                                    <input type='field' placeholder="Opponent's Name"  id='first-name-field' class='textbox' name='opponent-name'/>
+                                    <input type='field' placeholder="Opponent's Name"  id='first_name_field' class='textbox' name='opponent_name'/>
                                 </p>
                                 <p>
-                                    <input type='field' placeholder="Opponent's Address" id='opponent-address-field' class='textbox text-box-spec' name
-                                    ='opponent-address'/>
+                                    <input type='field' placeholder="Opponent's Address" id='opponent_address_field' class='textbox text_box_spec' name='opponent_address'/>
                                 </p>
+                                 <p>
+                                    <input type='field' placeholder="Opponent's City" id='opponent_city_field' class='textbox' name='opponent_city'/>
+                                     <input type='field' placeholder="Opponent's State" id='opponent_state_field' class='textbox' name='opponent_state'/>
+                                </p>
+                                 <p>
+                                    <input type='field' placeholder="Opponent's Country" id='opponent_country_field' class='textbox' name='opponent_country'/>
+                                     <input type='field' placeholder="Opponent's Zip Code" id='opponent_zipcode_field' class='textbox' name='opponent_zipcode'/>
+                                </p>
+                                
                                 <p>
-                                    <input type='field' placeholder="Opponent's Phone" id='opponent-phone-field' class='textbox text-box-spec'
+                                    <input type='field' placeholder="Opponent's Phone" id='opponent-phone-field' class='textbox text_box_spec'
                                      name='opponent-phone'/>
                                 </p>
                                 <p>
-                                    <input type='field' placeholder="Opponent's Email Address" id='opponent-email-field' class='textbox text-box-spec' 
-                                    name='opponent-email'/>
+                                    <input type='field' placeholder="Opponent's Email Address" id='opponent_email_field' class='textbox text_box_spec' 
+                                    name='opponent_email'/>
                                 </p>
                                 <p>
-                                    <textarea rows="5" cols="50" placeholder="What has Happened?" id="scroll" class="textbox report-textarea"></textarea>
+                                    <textarea rows="5" cols="50" placeholder="What has Happened?" id="scroll" class="textbox report_textarea"></textarea>
                 
                                 </p>
                                 
-                                <select  class="select" name="category" ><!--no size will be a drop down list-->
+                                <select class="select" name="category" ><!--no size will be a drop down list-->
                                 	<option value="industry">Industry</option>
                                 	<option value="retailstore">RetailStore</option>
                                    	<option value="transportation">Transportation</option> 
