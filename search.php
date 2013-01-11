ca40102a24a179d515c5699aec96236bc37fad0e
wu <panwu5588@yahoo.com>
2013-01-11 12:56:08 -0500

    search--nothing added

---
+++ search.php
@@ -0,0 +1,21 @@
+<!-- header -->
+<?php
+	include("include/header.php");
+?>
+
+<!---------->
+<!-- MENU -->
+<!---------->
+<div id='full_content'>
+	<?php include("left-menu.php"); ?>
+    
+    <?php include("main-content.php"); ?>
+    
+    <?php include("right-content.php"); ?>
+</div>
+
+
+<!-- header -->
+<?php
+	include("include/footer.php");
+?>
