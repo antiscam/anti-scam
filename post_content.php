0596467ba9d976fbea229be59737066a161cb715
ikhlas ahmed <ikhlas_06@hotmail.com>
2013-02-01 12:11:46 -0500

    comment2

--- post_content.php
+++ post_content.php
@@ -1,24 +1,34 @@
+<!------------------->
 <!-- add comment -->
+<!------------------>
 <?php
 	if(isset($_GET['r_id']))
 	{
+		$report_id_g = $_GET['r_id'];
 		if(isset($_POST['add_comment']))
 		{
-			$from_id_c = $_COOKIE['id'];
-			$report_id = $_GET['r_id'];
-			$comment_p = $_POST['comment'];
+			//if(!(empty($_post['comment'])))
+				//{
+					$from_id_c = $_COOKIE['id'];
+					
+					$comment_p = $_POST['comment'];
+					echo"$comment_p";
+					$report_query_s = mysql_query("SELECT * FROM report WHERE report_id = $report_id_g")or die(mysql_error());  
+					$row = mysql_fetch_assoc($report_query_s);
+						$to_id_r = $row['user_id'];
+						$report_type_r = $row['report_type'];
 			
-			$user_query = mysql_query("SELECT * FROM report WHERE report_id = $r_id")or die(mysql_error());  
-			$row = mysql_fetch_assoc($user_query);
-				$to_id_r = $row['user_id'];
-	
-		$register = mysql_query("INSERT INTO comment (to_id, from_id, report_type, comment, likes, dislike, date)
-									VALUES('$to_id_r', '$from_id_c', '', '$comment_p', 'now()')")
-													 or die(mysql_error());
+					$comment_insert = mysql_query("INSERT INTO comment (to_id, from_id, report_type, comment, likes, dislike, date)
+											VALUES('$to_id_r', '$from_id_c', '$report_type_r', '$comment_p', '0', '0', 'now()')")
+															 or die(mysql_error());
+				//}
 		}
-
+//stop from adding double on refresh
+	//header("location: post.php?r_id='$report_id_g'");
+	//exit();
 	}
 ?>
+
 
 
 
@@ -108,7 +118,7 @@ if((isset($_GET['r_id'])))
 											<div class='address_'>Address: $address_db</div>
 											<div class='phone_'>Phone: $phone_db</div>
 											<div class='email_'>Email: $email_db</div>
-											<div class='account_'>Acount:</div>
+											<div class='account_'>Acount: $name_id_db</div>
 										</div>
 										<div class='separatingLine'></div>
 										<div class='story'>
@@ -122,25 +132,36 @@ if((isset($_GET['r_id'])))
 							if((isset($_COOKIE['firstname'])))
 							{
 								echo"
+								<div id='comment' border='1px'>
 									<form action='post.php?r_id=$r_id_g' method='POST'>
 										 <textarea class='textarea' name='comment'></textarea>
 										<button type='submit' id='reportButton' class='button reportButton' name='add_comment' >Reply</button>
 									</form>
+									</div>
 								";
 							}
 							
 							
 							//display Comment
+							$comment_query = mysql_query("SELECT * FROM comment WHERE report_type = '$report_type_db' ORDER BY comment_id DESC")or die(mysql_error());  
+							while($row = mysql_fetch_assoc($comment_query))
+							{
+								$to_id_db = $row['to_id'];
+								$from_id_db = $row['from_id'];
+								$comment_db = $row['comment'];
+								$likes_c_db = $row['likes'];
+								$dislike_c_db = $row['dislike'];
+								$date_c_db = $row['date'];
 							echo
 							"
 							<div class='user_comments'>
 								<div class='separatingLine' style='clear:both;'></div>
 								<div class='postHead'>                            
 									<div class='date'>
-										 Posted at 19 February 2006 - 07:30 PM
+										 Posted at $date_c_db
 									</div>      
 									<div class='avoid'>
-										<a class='clickToIncrease' href='#'>Likes:</a>200
+										<a class='clickToIncrease' href='#'>Likes:</a>$likes_db
 									</div>
 								</div>
 								<div class='userInfo'>
@@ -154,11 +175,11 @@ if((isset($_GET['r_id'])))
 									</div>
 								</div>
 								<div class='postContentRight'>
-									<div class='comments'>Here is the commentHere is the commentHere is the commentHere is the comment</div>
+									<div class='comments'>$comment_db</div>
 								</div>
 							</div>
 							";
-							
+							}
 							
 					echo"		                  
 						</div>                    
